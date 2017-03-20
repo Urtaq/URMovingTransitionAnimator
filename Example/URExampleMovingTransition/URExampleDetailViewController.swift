0d7c8656e935d09a1703658259c4ad324f79c55e
Urtaq <jegumhon@gmail.com>
2017-03-20 17:05:53 +0900

    update pod spec

--- Example/URExampleMovingTransition/URExampleDetailViewController.swift
+++ Example/URExampleMovingTransition/URExampleDetailViewController.swift
@@ -34,4 +34,15 @@ class URExampleDetailViewController: UIViewController, URMovingTransitionReceiva
 
         return finishingFrame
     }
+    @IBAction func changImg(_ sender: Any) {
+        let anim = CABasicAnimation(keyPath: "contents")
+        anim.fromValue = #imageLiteral(resourceName: "suzy1").cgImage
+        anim.toValue = #imageLiteral(resourceName: "sulhyun1").cgImage
+        anim.duration = 0.5
+        anim.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        anim.fillMode = kCAFillModeForwards
+        anim.isRemovedOnCompletion = false
+
+        self.imgView.layer.add(anim, forKey: nil)
+    }
 }
